1989a186d32800986f7779a1151b52a11093c7cc
LOHAMER <130869756+LO-HAMER@users.noreply.github.com>
2024-02-06 21:00:33 +0100

    main.js

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 const menu = {
-  _meal: '',
-  _price: 0,
+  _meal: 'wrap',
+  _price: 7,
 
   set meal (mealToCheck) {
   if (typeof mealToCheck === 'String') {
-    return this._meal = mealToCheck
+    return this._meal = mealToCheck;
   }
-}
-}
+},
 
+ set price (priceToCheck) {
+  if (typeof priceToCheck === 'number') {
+     return this._price = priceToCheck;
+    }
+ },
+
+ get todaysSpecial () {
+  if (this._meal && this._price) {
+   return `Today’s Special is ${this._meal} for ${this._price}$!`
+  } else {
+    return 'Meal or price was not set correctly!'
+  }
+ }
+};
+
+//menu._meal = 'wrap';
+//menu._price = 7;
+console.log(menu.todaysSpecial);
 
